b701adf65f497a5a8121637339a27ff83c8ce85d
DavidContrerasGarza <david.contreras@rgvsoluciones.com>
2026-01-07 20:51:40 -0600

    Minor changes

--- static/js/system/input_table/update_record.js
+++ static/js/system/input_table/update_record.js
@@ -9,16 +9,16 @@
         .then(response => response.json())
         .then(data => {
             if (data.message !== "" && data.status==='warning') {
-                window.dispatchEvent(new CustomEvent('show-warning', {
+                window.dispatchEvent(new CustomEvent('show-info', {
                     detail: data.message   // the message from your AJAX or backend
                 }));
-                warningAlert.style.display = 'flex';  // Show the warning message
-                warningAlert.style.opacity = '1';  // Set opacity to 1 for showing
+                warningInfo.style.display = 'flex';  // Show the warning message
+                warningInfo.style.opacity = '1';  // Set opacity to 1 for showing
                 setTimeout(function() {
-                    warningAlert.style.opacity = '0';  // Start fading out
+                    warningInfo.style.opacity = '0';  // Start fading out
                     // After the fade-out animation is complete, hide the element
                     setTimeout(function() {
-                        warningAlert.style.display = 'none';
+                        warningInfo.style.display = 'none';
                     }, 1000);  // Match this delay with the CSS transition time (1s)
                 }, 3000);
                 if (data.value!=''){
